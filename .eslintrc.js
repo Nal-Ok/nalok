850e3f5164665e5fcf5245e0e6998f0bdf39d5a2
Nal-Ok <gpwjddl25@gmail.com>
2024-11-21 13:56:45 +0900

    첫번째 커밋

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    extends: ["react-app", "plugin:react/recommended"]
+  };
---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  rules:{
+    "no-console" : "error",
+    "no-unused-vars" : "warn",
+    "no-undef": "errors",
+    "eqeqeq" : "errors",
+    "no-debugger" :  "error",
+    "semi" : ["error", "always"],
+    "quotes" : ["error", "single"],
+    "indent" : ["error",2],
+    "comma-dangle": ["error", "always-muliline"],
+    "react/jsx-users-react": "off",
+    "react/prop-types": "off",
+  }
+  },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
